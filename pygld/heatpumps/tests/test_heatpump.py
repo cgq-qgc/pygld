9afd38a172b8e8731d508a5436d773333360c66b
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-07-07 00:44:27 -0400

    Update tests to the latest changes

--- pygld/heatpumps/tests/test_heatpump.py
+++ pygld/heatpumps/tests/test_heatpump.py
@@ -13,6 +13,7 @@ import os
 # ---- Third party imports
 
 import pytest
+import numpy as np
 
 # ---- Local imports
 
@@ -29,36 +30,22 @@ def test_heatpump_init():
 
     # Assert the default values for the independent variables.
 
-    propnames = ['TinHP', 'qbat', 'Vf']
-    expected_vals = [(28, 0), (16.5, 14.5), (0.94635295, 0.94635295)]
-    for (propname, expected_val) in zip(propnames, expected_vals):
-        prop = getattr(heatpump, propname)
-        assert id(prop) == id(getattr(heatpump, propname))
-
-        assert (prop.c == prop.cooling == prop['c'] == prop['cooling'] ==
-                prop[0] == expected_val[0])
-        assert (prop.h == prop.heating == prop['h'] == prop['heating'] ==
-                prop[1] == expected_val[1])
-
+    assert np.array_equal(heatpump.TinHP, np.array([28, 0]))
+    assert np.array_equal(heatpump.qbat, np.array([-16.5, 14.5]))
+    assert np.array_equal(heatpump.Vf, np.array([0.94635295, 0.94635295]))
     assert heatpump.fluid == 'water'
     assert heatpump.fr == 0
 
     # Assert the default values for the dependent variables.
 
-    propnames = ['COP', 'CAP', 'ToutHP', 'Tm']
-    expected_vals = [(3.849727, 3.372328), (20.926771, 17.196401),
-                     (33.273623, -2.555170), (30.636812, -1.277585)]
-    for (propname, expected_val) in zip(propnames, expected_vals):
-        prop = getattr(heatpump, propname)
-        assert id(prop) == id(getattr(heatpump, propname))
-
-        assert (round(prop.c, 6) == round(prop.cooling, 6) ==
-                round(prop['c'], 6) == round(prop['cooling'], 6) ==
-                round(prop[0], 6) == expected_val[0])
-
-        assert (round(prop.h, 6) == round(prop.heating, 6) ==
-                round(prop['h'], 6) == round(prop['heating'], 6) ==
-                round(prop[1], 6) == expected_val[1])
+    assert np.array_equal(np.round(heatpump.COP, 6),
+                          np.array([3.849727, 3.372328]))
+    assert np.array_equal(np.round(heatpump.CAP, 6),
+                          np.array([20.926771, 17.196401]))
+    assert np.array_equal(np.round(heatpump.ToutHP, 6),
+                          np.array([33.273623, -2.555170]))
+    assert np.array_equal(np.round(heatpump.Tm, 6),
+                          np.array([30.636812, -1.277585]))
 
 
 def test_heatpump_need_update_flags():
@@ -79,17 +66,10 @@ def test_heatpump_need_update_flags():
     for key, value in heatpump._need_update.items():
         assert value is False
 
-    heatpump.TinHP.c = 34
+    heatpump.TinHP = 34
 
     for key, value in heatpump._need_update.items():
         assert value is True
-
-
-def test_calcul_Tm():
-    """Test that the average fluid temperature is calculated correctly."""
-    heatpump = HeatPump()
-    assert 0.5 * (heatpump.TinHP.c + heatpump.ToutHP.c) == heatpump.Tm.c
-    assert 0.5 * (heatpump.TinHP.h + heatpump.ToutHP.h) == heatpump.Tm.h
 
 
 if __name__ == "__main__":
