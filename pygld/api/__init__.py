f8cd4d61c3c7d1e00adbe7e2890396dd448bc182
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-07-13 08:26:44 -0400

    Make file org more flat

--- pygld/api/__init__.py
+++ pygld/api/__init__.py
@@ -7,6 +7,9 @@
 # Licensed under the terms of the GNU General Public License.
 
 """
-This is an object-oriented plotting library for designing coupled
+This is an object-oriented library for modeling coupled
 ground-loop heat exchanger.
 """
+import os.path as osp
+from pygld import __rootdir__
+__datadir__ = osp.join(__rootdir__, 'api', 'data')
